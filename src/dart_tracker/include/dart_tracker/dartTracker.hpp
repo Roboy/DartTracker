3d6b32d6ddf972fb494e62d0966f7f915e09b9de
letrend <simon.trendel@tum.de>
2017-05-11 14:17:42 +0200

    first draft rviz plugin

---
+++ src/dart_tracker/include/dart_tracker/dartTracker.hpp
@@ -0,0 +1,125 @@
+#ifndef Q_MOC_RUN
+// qt
+#include <QPainter>
+#include <QCheckBox>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QTableWidget>
+#include <QComboBox>
+#include <QTimer>
+#include <QScrollArea>
+#include <QListWidget>
+#include <QStyledItemDelegate>
+// std
+#include <map>
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <Eigen/Dense>
+
+#include <cuda_runtime.h>
+#include <vector_types.h>
+
+#include "depth_sources/image_depth_source.h"
+#include "geometry/plane_fitting.h"
+#include "img_proc/img_ops.h"
+#include "optimization/priors.h"
+#include "tracker.h"
+#include "util/dart_io.h"
+#include "util/gl_dart.h"
+#include "util/image_io.h"
+#include "util/ostream_operators.h"
+#include "util/string_format.h"
+#include "visualization/color_ramps.h"
+#include "visualization/data_association_viz.h"
+#include "visualization/gradient_viz.h"
+#include "visualization/sdf_viz.h"
+// ros
+#include <ros/ros.h>
+#include <rviz/panel.h>
+#include <pluginlib/class_loader.h>
+#include <pluginlib/class_list_macros.h>
+#include <visualization_msgs/Marker.h>
+#include <std_srvs/SetBool.h>
+
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#endif
+
+using namespace std;
+using namespace Eigen;
+
+#define EIGEN_DONT_ALIGN
+
+enum PointColorings {
+    PointColoringNone = 0,
+    PointColoringRGB,
+    PointColoringErr,
+    PointColoringDA,
+    NumPointColorings
+};
+
+enum DebugImgs {
+    DebugColor=0,
+    DebugObsDepth,
+    DebugPredictedDepth,
+    DebugObsToModDA,
+    DebugModToObsDA,
+    DebugObsToModErr,
+    DebugModToObsErr,
+    DebugJTJ,
+    DebugN
+};
+
+enum TrackingMode {
+    ModeObjOnTable,
+    ModeIntermediate,
+    ModeObjGrasped,
+    ModeObjGraspedLeft
+};
+
+const static int panelWidth = 180;
+
+static const int fullArmFingerTipFrames[10] = { 11, 15, 19, 23, 27,  38, 42, 46, 50, 54 };
+static const int handFingerTipFrames[5] = { 4, 8, 12, 16, 20 };
+
+class DartTracker : public rviz::Panel{
+    Q_OBJECT
+public:
+    DartTracker();
+    ~DartTracker();
+    /**
+    * Load all configuration data for this panel from the given Config object.
+    * @param config rviz config file
+    */
+    virtual void load(const rviz::Config &config){};
+
+    /**
+     * Save all configuration data from this panel to the given
+     * Config object.  It is important here that you call save()
+     * on the parent class so the class id and panel name get saved.
+     * @param config rviz config file
+     */
+    virtual void save(rviz::Config config) const{};
+
+private:
+    ros::NodeHandlePtr nh;
+    boost::shared_ptr<ros::AsyncSpinner> spinner;
+    dart::Tracker *tracker;
+    dart::ParamMapPoseReduction *poseReduction;
+    float defaultModelSdfPadding = 0.07;
+};
---
+++ src/dart_tracker/src/dartTracker.cpp
@@ -0,0 +1,27 @@
+#include "dart_tracker/dartTracker.hpp"
+
+DartTracker::DartTracker(){
+    cudaSetDevice(0);
+    cudaDeviceReset();
+    tracker = new dart::Tracker;
+
+    poseReduction = dart::loadParamMapPoseReduction("/home/roboy/workspace/dart_tracker/src/dart_tracker/models/spaceJustin/justinHandParamMap.txt");
+
+    const static int obsSdfSize = 64;
+    const static float obsSdfResolution = 0.01*32/obsSdfSize;
+    const static float defaultModelSdfResolution = 2e-3; //1.5e-3;
+    const static float3 obsSdfOffset = make_float3(0,0,0.1);
+
+    tracker->addModel("/home/roboy/workspace/dart_tracker/src/dart_tracker/models/spaceJustin/spaceJustinHandRight.xml",
+                      defaultModelSdfResolution,
+                      defaultModelSdfPadding,
+                     obsSdfSize,
+                     obsSdfResolution,
+                     make_float3(-0.5*obsSdfSize*obsSdfResolution) + obsSdfOffset, poseReduction);
+}
+
+DartTracker::~DartTracker(){
+
+}
+
+PLUGINLIB_EXPORT_CLASS(DartTracker, rviz::Panel)
